2b619f449af4161d49ba9add86a863b16f0a9f69
dennis1611 <dennis16112000r@gmail.com>
2017-10-15 15:28:44 +0200

    Update ViewFileActivity.java

--- app/src/main/java/com/gfo/gfo_meesterproef/Admin/ViewFileActivity.java
+++ app/src/main/java/com/gfo/gfo_meesterproef/Admin/ViewFileActivity.java
@@ -2,19 +2,37 @@ package com.gfo.gfo_meesterproef.Admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.gfo.gfo_meesterproef.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class ViewFileActivity extends AppCompatActivity {
+
+    private ListView fileList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_file);
 
+//        contact database
         String type = "view";
-        com.gfo.gfo_meesterproef.Admin.ViewGroupBackgroundWorker backgroundWorker = new com.gfo.gfo_meesterproef.Admin.ViewGroupBackgroundWorker(this);
-        backgroundWorker.execute(type);
-
+        List<String> groups = new ArrayList<String>();
+        try {
+            groups = new ViewGroupBackgroundWorker(this).execute(type).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+//        fill listView with (array)List
+        fileList = (ListView) findViewById(R.id.fileList);
+        ArrayAdapter<String> groupAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groups);
+        fileList.setAdapter(groupAdapter);
     }
 }
