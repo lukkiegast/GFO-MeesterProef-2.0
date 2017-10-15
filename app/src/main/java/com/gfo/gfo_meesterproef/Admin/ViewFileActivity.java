f0907361349db2218be2756383b66372f952702d
dennis1611 <dennis16112000r@gmail.com>
2017-10-15 17:20:57 +0200

    Update ViewFileActivity.java

--- app/src/main/java/com/gfo/gfo_meesterproef/Admin/ViewFileActivity.java
+++ app/src/main/java/com/gfo/gfo_meesterproef/Admin/ViewFileActivity.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ExecutionException;
 
 public class ViewFileActivity extends AppCompatActivity {
 
-    ListView fileList;
+    ListView adminGroupList;
     String selectedGroup;
 
     @Override
@@ -36,21 +36,21 @@ public class ViewFileActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 //        fill listView with (array)List
-        fileList = (ListView) findViewById(R.id.fileList);
+        adminGroupList = (ListView) findViewById(R.id.adminGroupList);
         ArrayAdapter<String> groupAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groups);
-        fileList.setAdapter(groupAdapter);
+        adminGroupList.setAdapter(groupAdapter);
 
         registerGroupClickCallback();
     }
 
 //    select group
     private void registerGroupClickCallback() {
-        fileList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        adminGroupList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
                 TextView textView = (TextView) viewClicked;
                 selectedGroup = textView.getText().toString();
-                
+
                 Toast.makeText(ViewFileActivity.this,
                         selectedGroup, Toast.LENGTH_LONG).show();
             }
